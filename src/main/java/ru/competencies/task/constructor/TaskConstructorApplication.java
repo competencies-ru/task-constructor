c2585ea01fc02ddeab326d5a8b715ab9018ac12a
Max Tsilyurik <tsilyurik.maxim@gmail.com>
2022-06-28 19:50:12 +0300

    added spring boot in project

---
+++ src/main/java/ru/competencies/task/constructor/TaskConstructorApplication.java
@@ -0,0 +1,11 @@
+package ru.competencies.task.constructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskConstructorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskConstructorApplication.class, args);
+    }
+}
